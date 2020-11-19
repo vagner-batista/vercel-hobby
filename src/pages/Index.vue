0bbd04fba42de8a8e6afbba072c013f640562ebb
Vagner Batista <vagnereletronica@gmail.com>
2020-11-19 10:56:01 -0300

    Início do layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,8 +4,26 @@
       <div class="col-8  self-center">
         <p>Aqui ficará o canvas para stream da câmera</p>
       </div>
-      <div class="col-grow col-sm-3">
-        <p>Aqui ficarão os snapshots recolhidos da câmera</p>
+      <div class="q-pa-md">
+        <q-carrousel
+          transition-prev="slide-right"
+          transition-next="slide-left"
+          swipeable
+          animated
+          control-color="primary"
+          navigation
+          padding
+          arrows
+          height="300px"
+          class="bg-grey-1 shadow-2 rounded-borders"
+        >
+        <q-carousel-slide :name="1" class="column no-wrap">
+          <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
+            <q-img class="rounded-borders col-6 full-height" />
+            <q-img class="rounded-borders col-6 full-height" />
+          </div>
+        </q-carousel-slide>
+      </q-carrousel>
       </div>
     </div>
   </q-page>
