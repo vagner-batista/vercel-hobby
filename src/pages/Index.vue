b113999d17b4e2ad4e5018bfb77695c10772d31c
Vagner Batista <vagnereletronica@gmail.com>
2020-11-19 11:04:15 -0300

    Adicionado imagens para placeholders

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,8 +19,10 @@
         >
         <q-carousel-slide :name="1" class="column no-wrap yellowish">
           <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-            <q-img class="rounded-borders col-6 full-height redish" />
-            <q-img class="rounded-borders col-6 full-height redish" />
+            <q-img class="rounded-borders col-6 full-height redish" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+            <q-img class="rounded-borders col-6 full-height redish" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+            <q-img class="rounded-borders col-6 full-height redish" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+            <q-img class="rounded-borders col-6 full-height redish" src="https://cdn.quasar.dev/img/linux-avatar.png" />
           </div>
         </q-carousel-slide>
       </q-carrousel>
