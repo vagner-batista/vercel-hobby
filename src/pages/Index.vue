04c5a3898db0d75aef163cf7823f555dac099d0d
Vagner Batista <vagnereletronica@gmail.com>
2020-11-19 10:44:17 -0300

    Início do layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <div class="full-width row wrap justify-center items-center content-center">
-    	<div class="col-8  self-center">
-    		<p>Aqui ficará o canvas para stream da câmera</p>
-    	</div>
-    	<div class="col-grow col-sm-3">
-    		<p>Aqui ficarão os snapshots recolhidos da câmera</p>
-    	</div>
+      <div class="col-8  self-center">
+        <p>Aqui ficará o canvas para stream da câmera</p>
+      </div>
+      <div class="col-grow col-sm-3">
+        <p>Aqui ficarão os snapshots recolhidos da câmera</p>
+      </div>
     </div>
   </q-page>
 </template>
